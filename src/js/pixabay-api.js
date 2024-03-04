b13bdd6c624d8cae69502814efdb58612e746f3e
OrestZa <119166956+OrestZa@users.noreply.github.com>
2024-03-04 18:50:37 +0100

    3 days and find error

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,28 @@
-const BASE_API = 'https://pixabay.com/api/';
-const KEY = '42653068-ca30bcafd67c2a07aae2cb182';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 
-function getImages(query) {
-    const LINK = `${BASE_API}?key=${KEY}&q=${query}`;
+const loaderEl = document.querySelector('.loader');
+
+
+
+export function getImages(query) {
+
+  const options = {
+    base: 'https://pixabay.com/api/',
+    key: '42653068-ca30bcafd67c2a07aae2cb182',
+    q: '',
+    image_type: 'photo',
+    oriental: 'horizontal',
+    safesearch: 'true',
+  };
+
+  options.q = query;
+  const params = new URLSearchParams(options);
+
+  loaderEl.style.display = 'block';
+
+  const LINK = `${options.base}?${params}`;
   return fetch(LINK)
     .then(res => {
       if (!res.ok) {
@@ -11,6 +30,19 @@ function getImages(query) {
       }
       return res.json();
     })
+    .then(data => {
+      loaderEl.style.display = 'none';
+      if (data.hits.length === 0) {
+        iziToast.error({
+          timeout: 1500,
+          position: 'bottomCenter',
+          message:
+            'There are no images matching your search query. Please, enter something else!',
+        });
+      }
+      return data;
+    })
+    .catch(error => {
+      console.log('Error fetching data!', error);
+    });
 }
-
-getImages().then(data => console.log(data));
---
+++ src/main.js
@@ -0,0 +1,39 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+import { getImages } from './js/pixabay-api';
+import { searchImage } from './js/render-functions';
+
+const formEl = document.querySelector('.search');
+const galleryEl = document.querySelector('.gallery');
+
+
+let query = '';
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+      nav: true,
+      captions: true,
+      captionsData: 'alt',
+      captionDelay: 150,
+    });
+
+  query = formEl.elements.query.value.trim();
+    galleryEl.innerHTML = '';
+
+    getImages(query)
+      .then(data => {
+        const markup = searchImage(data);
+        galleryEl.insertAdjacentHTML('beforeend', markup);
+        lightbox.refresh();
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
+    formEl.reset();
+}
